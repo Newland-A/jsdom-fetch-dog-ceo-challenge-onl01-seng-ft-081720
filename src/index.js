b76d16fb0f5fa18a3d6cff9930b7164774ae156d
Newland-A <ajn252@gmail.com>
2020-11-30 03:27:58 -0500

    commit tree 856a1724b2a407596f18187c90a82adaf027c229

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  
+  const breedSelect = document.getElementById("breed-dropdown")
+  const breedList = document.getElementById('dog-breeds')
+  fetchImages()
+  
+})
+
+function fetchImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+  .then(resp => resp.json())
+  .then(json => showImagesToPage(json))
+}
+
+function showImagesToPage(pics) {
+  const picContainer = document.getElementById('dog-image-container')
+  pics.message.forEach(pic => {
+  const img = document.createElement('img')
+  img.src = pic
+      picContainer.appendChild(img)
+  });
+}
+
+fetchBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+  .then(resp => resp.json())
+}
+
